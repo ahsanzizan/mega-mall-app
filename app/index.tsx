2b408160ecdb57a48b7a81a8207bae3c27e59bc0
Ahsan <ahsanaz461@gmail.com>
2023-11-07 09:22:00 +0700

    feat: add marketing card in home page

--- app/index.tsx
+++ app/index.tsx
@@ -32,6 +32,30 @@ export default function RootPage() {
 
   return (
     <ScrollView className="p-6">
+      <View
+        className="w-full mb-7 flex flex-row justify-between relative"
+        style={{ borderRadius: 10 }}
+      >
+        <View
+          className="bg-[#3669C9] py-8 px-6"
+          style={{
+            borderTopLeftRadius: 10,
+            borderBottomLeftRadius: 10,
+            borderTopRightRadius: 100,
+            borderBottomRightRadius: 100,
+          }}
+        >
+          <Text className="text-white font-bold text-xl mb-2">
+            Gratis Ongkir{"\n"}Selama PPKM!
+          </Text>
+          <Text className="text-white">Periode Mei - Agustus 2020</Text>
+        </View>
+        <Image
+          source={require("../assets/images/cover.png")}
+          className="absolute right-0 -z-10 h-full"
+          style={{ borderTopRightRadius: 10, borderBottomRightRadius: 10 }}
+        />
+      </View>
       <View className="mb-5">
         <View className="mb-5">
           <View className="flex flex-row justify-between">
