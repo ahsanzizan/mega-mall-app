64af66576ed2094f45183d1e684a802bf81c5b4e
Ahsan <ahsanaz461@gmail.com>
2023-11-07 23:21:39 +0700

    feat: finish news section

--- app/index.tsx
+++ app/index.tsx
@@ -13,19 +13,25 @@ import SectionHeader from "../components/SectionHeader";
 import MarketingCard from "../components/MarketingCard";
 import CategoryTag from "../components/CategoryTag";
 import SpecialProductCard from "../components/SpecialProductCard";
+import { Post } from "../types/post.type";
+import { getAllPosts } from "../lib/queries/post.queries";
+import PostCard from "../components/PostCard";
 
 export default function RootPage() {
   const [products, setProducts] = useState<Product[] | null>(null);
   const [categories, setCategories] = useState<string[] | null>(null);
+  const [posts, setPosts] = useState<Post[] | null>(null);
 
   useEffect(() => {
     const init = async () => {
       try {
         const allProducts = await getAllProducts();
         const allCategories = await getAllCategories();
+        const allPosts = await getAllPosts();
 
         setProducts(allProducts);
         setCategories(allCategories);
+        setPosts(allPosts);
       } catch (error) {
         return false;
       }
@@ -88,6 +94,28 @@ export default function RootPage() {
                 })}
           </View>
         </ScrollView>
+      </View>
+      <View className="w-full">
+        <Text
+          className="text-base mb-14"
+          style={{ fontFamily: fontFamilies.medium }}
+        >
+          Latest News
+        </Text>
+        <View className="w-full mb-7">
+          <View className="flex flex-col gap-8">
+            {posts &&
+              posts
+                .slice(0, 5)
+                .map((post) => <PostCard key={post.id} post={post} />)}
+          </View>
+        </View>
+        <TouchableOpacity
+          className="w-full py-4 mb-20 border border-[#0C1A30]"
+          style={{ borderRadius: 10 }}
+        >
+          <Text className="text-center">See All News</Text>
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
---
+++ components/PostCard.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+import { Post } from "../types/post.type";
+import { fontFamilies } from "../styles/base";
+
+interface PostCardProps {
+  post: Post;
+}
+
+export default function PostCard({ post }: PostCardProps) {
+  return (
+    <View className="flex flex-row justify-between w-full items-start mb-8 pr-3 pl-8">
+      <View style={{ maxWidth: 225 }} className="flex flex-col gap-3">
+        <Text className="text-sm" style={{ fontFamily: fontFamilies.medium }}>
+          {post.title}
+        </Text>
+        <Text className="text-xs" style={{ fontFamily: fontFamilies.regular }}>
+          {post.body.substring(0, 35) + "..."}
+        </Text>
+        <Text className="text-[#838589] text-xs">13 Jan 2021</Text>
+      </View>
+      <Image
+        source={require("../assets/images/post-cover.png")}
+        style={{ width: 80, height: 80 }}
+      />
+    </View>
+  );
+}
